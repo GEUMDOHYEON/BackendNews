963e5b5586c14e4b940f0ad06d1726f1e8e48fe2
noa <kim71665646@gmail.com>
2024-09-24 19:45:05 +0900

    게시판 글 작성 API

--- routers/board.py
+++ routers/board.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, HTTPException
 from database import mysql_create_session
 from schemas.board import PostUpload_Model
 from datetime import datetime
@@ -15,10 +15,14 @@ router = APIRouter(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # 루트 경로에 .env파일로 값 설정
 load_dotenv(os.path.join(BASE_DIR, ".env"))
+# jwt에 필요한 전역변수
+SECRET_KEY = os.environ["SECRET_KEY"]
+# 알고리즘
+ALGORITHM = "HS256"
 
 # 게시판 글 작성
 @router.post("/postUpload")
-async def upload(response:Response, postUpload_data : PostUpload_Model):
+async def upload(postUpload_data : PostUpload_Model):
   # mysql 과 상호작용하기 위해 연결해주는 cur 객체
   conn, cur = mysql_create_session()
   
@@ -26,14 +30,27 @@ async def upload(response:Response, postUpload_data : PostUpload_Model):
   token_Info = jwt.decode()
   
   # 유저 정보 - 토큰 디코딩 후 - 아이디, 닉네임 가져오기
-  nickname = jwt.decode(postUpload_data.access_token, os.environ[SECRET_KEY], argorithms = ALGORITHM)
+  user_nickname = jwt.decode(postUpload_data.access_token, SECRET_KEY, argorithms = [ALGORITHM])
   
   # 게시판 정보 - 제목, 내용, 작성 시간
-  title = postUpload_data.community_title
-  content = postUpload_data.community_content
+  community_title = postUpload_data.community_title
+  community_content = postUpload_data.community_content
   
   now = datetime.now()
-  createat = now.strftime("%Y-%m-%d")
+  community_createat = now.strftime("%Y-%m-%d")
+  
+  try:
+    # 
+    sql = "INSERT INTO `USER`.`Community` (`community_title`, `user_nickname`, `community_content`, `community_createat`) VALUES (%s, %s, %s, %s);"
+    cur.execute(sql,(community_title,user_nickname,community_content,community_createat))
+    conn.commit()
+  except Exception as e:
+    conn.rollback()
+    # 에러 발생시 예외 메세지 (detail)를 전달 
+    raise HTTPException(status_code=400, detail=str(e))
+  finally:
+    conn.close()
+  
   
   return {"업로드 성공"}
 
@@ -47,3 +64,5 @@ async def remove():
 @router.post("/post")
 async def update():
   return {"수정 성공"}
+
+# 게시판 글 불러오기
