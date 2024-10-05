6fc505b460a0b4233539ac6cd73945140ce1651d
Coda <onemore4111@gmail.com>
2024-10-05 16:27:52 +0900

    로깅기능&개인키워드 추가/조회/삭제

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, BackgroundTasks
 from routers import users, news,board
 # CORS 정책
 from fastapi.middleware.cors import CORSMiddleware
 # 백그라운드 스케줄러
 from apscheduler.schedulers.background import BackgroundScheduler
 import os
+# 로깅
+import logging
+from datetime import datetime
 
 from scheduler import get_news_from_api
 from contextlib import asynccontextmanager
@@ -12,21 +15,21 @@ from contextlib import asynccontextmanager
 # 백그라운드 스케줄링 작업
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-  # 서버가 구동되면 실행되는 코드
+#   # 서버가 구동되면 실행되는 코드
 
-  scheduler = BackgroundScheduler()
-  # 1시간마다 새로운 뉴스를 가져오기 위해 뉴스 스크래핑 작업 진행
-  scheduler.add_job(get_news_from_api, 'interval', hours=1)
-  scheduler.start()
-  # 서버 실행 후 곧바로 뉴스 스크래핑 작업 진행
-  # 잠시 켜둠 '다시 주석처리'
-  # get_news_from_api()
+#   scheduler = BackgroundScheduler()
+#   # 1시간마다 새로운 뉴스를 가져오기 위해 뉴스 스크래핑 작업 진행
+#   scheduler.add_job(get_news_from_api, 'interval', hours=1)
+#   scheduler.start()
+#   # 서버 실행 후 곧바로 뉴스 스크래핑 작업 진행
+#   # 잠시 켜둠 '다시 주석처리'
+#   # get_news_from_api()
 
   yield
 
-  #서버가 종료되면 실행되는 코드
+#   #서버가 종료되면 실행되는 코드
 
-  scheduler.shutdown()
+#   scheduler.shutdown()
 
 
 # swagger 페이지 소개
@@ -42,6 +45,19 @@ SWAGGER_HEADERS = {
 }
 
 app = FastAPI(lifespan=lifespan, **SWAGGER_HEADERS)
+
+
+# 로깅 설정
+logging.basicConfig(filename='info.log', level=logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logging.getLogger().addHandler(console_handler)
+
+
+# 백그라운드 작업으로 처리할 로깅 함수
+def log_request_info(req_method: str, req_url: str):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info(f"[{current_time}] Request: {req_method} {req_url}")
 
 
 # CROS 허용 ip
@@ -68,6 +84,24 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(news.router)
 app.include_router(board.router)
+
+
+# Request 로깅 미들웨어
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+
+    # 요청 메서드와 URL 로깅
+    req_method = request.method
+    req_url = str(request.url)
+
+    # 요청 처리 후 바로 응답 반환
+    response = await call_next(request)
+
+    # 요청 정보를 백그라운드 로깅
+    Background_Tasks = BackgroundTasks()
+    Background_Tasks.add_task(log_request_info(req_method, req_url))
+
+    return response
 
 @app.get("/")
 def helloWorld():
