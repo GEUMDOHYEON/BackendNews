ed5b21468d6a41050bd1efd450ee6215a21c06e0
noa <kim71665646@gmail.com>
2024-10-07 01:55:01 +0900

    게시판 API 마무리

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ async def lifespan(app: FastAPI):
   scheduler.add_job(get_news_from_api, 'interval', hours=1)
   scheduler.start()
   # 서버 실행 후 곧바로 뉴스 스크래핑 작업 진행
+  # 테스트할 때 확인하기
   get_news_from_api()
 
   yield
